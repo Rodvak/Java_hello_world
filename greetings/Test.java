ab549f025668f3f3a70cf1acde23f48cb1771d5c
Alex <av.rodriguez@outlook.com>
2022-04-01 23:22:25 -0700

    commit

---
+++ greetings/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("My name is Alejandro");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Los Angeles, CA");
+    }
+}
