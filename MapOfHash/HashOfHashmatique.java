0f86575bb8df95d3e231490413355dd2c5c8bfbe
Alex <av.rodriguez@outlook.com>
2022-04-05 01:07:11 -0700

    commit

--- MapOfHash/HashOfHashmatique.java
+++ MapOfHash/HashOfHashmatique.java
@@ -1,5 +1,6 @@
 // Import HashMap. HASHMAP are DICTIONARIES in Java.
 import java.util.HashMap;
+import java.util.Set;
 // ALWAYS create  the class and name it the SAME as the file.
 public class HashOfHashmatique {
     // Remember to always have public static void main(String[] args)
@@ -13,8 +14,9 @@ public class HashOfHashmatique {
         // In line 14 and 15, we created a variable = keyValue because we want to get the value of the key "My Hero". Then, we access the key value with dot notation and our userMap variable.get.
         String keyValue = userMap.get("My Hero");
         System.out.println(String.format("My Hero Lyrics: %s", keyValue));
-        // We use a for loop to get all the keys and values from our dictionary. We create a variable Key, we access our dictionary with userMap and .keySet.
-        for(String key : userMap.keySet()){
+        // We use a for loop to get all the keys and values from our dictionary. We create a variable Key, we access our dictionary with userMap and .keySet. KEYSET method returns a set of all the keys in our map. Then, in our for-loop, we iterate over the set, print the keys, and get the value in our map with said key.
+        Set<String> keys = userMap.keySet();
+        for(String key : keys){
             System.out.println(String.format("Track: %s Lyrics: %s", key, userMap.get(key)));
         }
     }
