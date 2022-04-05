fe51cff4a502ca1dad99006b8e2a4289974b67c8
Alex <av.rodriguez@outlook.com>
2022-04-04 22:59:38 -0700

    commit

---
+++ Puzzling/TestPuzzleJava.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class TestPuzzleJava{
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+        // Prints the assignment.
+		System.out.println(generator.getRandomLetter());
+		System.out.println(generator.generatePassword());
+		System.out.println(generator.getNewPasswordSet(10));
+
+		
+    	//..
+		// Write your other test cases here.
+		//..
+	}
+}
