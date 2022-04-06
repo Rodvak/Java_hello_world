2236c6463d7ffcaa991d3d04fec22c5a5bb7fbfc
Alex <av.rodriguez@outlook.com>
2022-04-05 17:21:49 -0700

    commit

---
+++ OrderAndItems/Order.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+
+public class Order{
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item> item = new ArrayList<Item>();
+}
+//     public Order(String name){
+//         this.name = name;
+//     }
+// }
+
+// See how THIS works
+// public void test(){
+//     System.out.println(this.name);
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
