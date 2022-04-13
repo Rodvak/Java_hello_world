a37a6ad00de5dc9d070238c77a91922e77f371d1
Alex <av.rodriguez@outlook.com>
2022-04-13 10:27:24 -0700

    commit

--- SpringProjects/bookapi/src/main/java/com/alex/bookapi/controllers/BooksApi.java
+++ SpringProjects/bookapi/src/main/java/com/alex/bookapi/controllers/BooksApi.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alex.bookapi.models.Book;
 import com.alex.bookapi.services.BookService;
 
-// text, xml, JSON
 @RestController
 public class BooksApi {
 
